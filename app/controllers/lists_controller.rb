65a12dcd86361daea82427fd7aa6b455ced932d9
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-07-03 04:56:32 +0000

    [start]lesson9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,9 +5,12 @@
   end
   
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(list.id)
+    else
+      render :new
+    end
   end
 
   def index
